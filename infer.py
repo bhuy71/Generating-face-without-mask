733d8f4d042bd8a31a46f2625dd1f8405a880252
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2024-12-15 19:12:00 +0700

    fixed

--- infer.py
+++ infer.py
@@ -209,7 +209,7 @@ def apply_bounding_box_mask(image_batch, model):
 
 # In[25]:
 
-
+!pip install diffusers
 import torch
 from diffusers import StableDiffusionInpaintPipeline
 from PIL import Image
